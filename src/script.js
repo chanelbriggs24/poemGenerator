2246a499a82eff5a8c3664ca4147272ecc2b6f6a
chanelbriggs24 <chanelbriggs24@gmail.com>
2024-07-19 18:55:50 -0400

    Imported axios and integrated AI api to the js script.

--- src/script.js
+++ src/script.js
@@ -1,14 +1,41 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log(response);
   let poem = document.querySelector("#display-poem");
-  let poemValue = (poem.innerHTML = `My dearest Jordan`);
+  let poemValue = response.data.answer;
   new Typewriter(poem, {
     strings: poemValue,
     autoStart: true,
-    cursor: null,
+    delay: null,
   });
-  console.log(poem);
+  console.log(poemValue);
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  poemTopic = document.querySelector("#poem-topic");
+
+  let apiKey = `abf74f3d08ac0ba0527t801bd8o47a65`;
+  let prompt = `Write a short and concise poem with the topic of ${poemTopic.value}.`;
+  console.log(poemTopic.value);
+  let context = `You are a world renowned poet. You can create any poem based on any topic`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+  let waitingMessage = document.querySelector("#display-poem");
+  waitingMessage.innerHTML = `Generating a poem...⏳`;
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
 poemFormElement.addEventListener("submit", generatePoem);
+
+function initialPoemDisplay(topic) {
+  let apiKey = `abf74f3d08ac0ba0527t801bd8o47a65`;
+  let prompt = `Write a short and concise poem with the topic of ${topic}.`;
+  console.log(topic);
+  let context = `You are a world renowned poet. You can create any poem based on any topic`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+initialPoemDisplay("Love");
