a1014183d3d2070755cbcd80557582c6bf566a58
chanelbriggs24 <chanelbriggs24@gmail.com>
2024-07-20 12:50:24 -0400

    Fixed the blinking animation for the waiting message. Updated the CSS

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 function displayPoem(response) {
   let poem = document.querySelector("#display-poem");
-  console.log(response.data.answer);
   let poemValue = response.data.answer;
   new Typewriter(poem, {
     strings: poemValue,
@@ -14,7 +13,7 @@ function generatePoem(event) {
   let poemTopic = document.querySelector("#poem-topic");
 
   let apiKey = `abf74f3d08ac0ba0527t801bd8o47a65`;
-  let prompt = `Write a poem with the topic of ${poemTopic.value} that is 5 lines long. Provide the poem in HTML without "html", without any leading or ending whitespaces.Sign every poem with <br> <strong> SheCodes AI at the end of the poem. </strong> `;
+  let prompt = `Write a poem with the topic of ${poemTopic.value} that is 5 lines long. Provide the poem in HTML without "html", without any leading or ending whitespaces.Sign every poem with <br> <strong> SheCodes AI </strong>  at the end of the poem. `;
   console.log(poemTopic.value);
   let context = `You are a world renowned poet. You can create any poem based on any topic. You follow instructions without fail.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
@@ -22,9 +21,7 @@ function generatePoem(event) {
   axios.get(apiUrl).then(displayPoem);
   let waitingMessage = document.querySelector("#display-poem");
   waitingMessage.classList.remove("hidden");
-  waitingMessage.innerHTML = `Generating a poem about ${poemTopic.value}⏳`;
-
-  console.log(waitingMessage);
+  waitingMessage.innerHTML = `<div class="blink">Generating a poem about ${poemTopic.value}⏳... </div>`;
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
@@ -32,7 +29,7 @@ poemFormElement.addEventListener("submit", generatePoem);
 
 function initialPoemDisplay(topic) {
   let apiKey = `abf74f3d08ac0ba0527t801bd8o47a65`;
-  let prompt = `Write a short and concise poem with the topic of ${topic} that is 5 lines long or less. Provide the poem in HTML without "html", without any leading or ending whitespaces. Sign every poem with <br> <strong> SheCodes AI at the end of the poem. </strong> .`;
+  let prompt = `Write a short and concise poem with the topic of ${topic} that is 5 lines long or less. Provide the poem in HTML without "html", without any leading or ending whitespaces. Sign every poem with <br> <strong> SheCodes AI </strong> at the end of the poem.  .`;
   let context = `You are a world renowned poet. You can create any poem based on any topic`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -40,4 +37,3 @@ function initialPoemDisplay(topic) {
 }
 
 initialPoemDisplay("Love");
-setInterval(generatePoem, 2000);
